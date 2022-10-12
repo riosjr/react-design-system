19e6c6a5583d2bed47b7eebcda35ec82da1ed93f
riosjr <riosjr@gmail.com>
2022-10-12 14:15:23 -0300

    feat: Heading component

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,11 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Text, TextProps } from './Text'
+import { Heading, HeadingProps } from './Heading'
 
 //global configurations
 
 export default {
-	title: 'Components/Text',
-	component: Text,
+	title: 'Components/Heading',
+	component: Heading,
 	args: {
 		children: 'Lorem ipsum.',
 		size: 'md',
@@ -18,28 +18,28 @@ export default {
 			},
 		},
 	},
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
 // varitans configurations
 
-export const Default: StoryObj<TextProps> = {}
+export const Default: StoryObj<HeadingProps> = {}
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
 	args: {
 		size: 'sm',
 	},
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
 	args: {
 		size: 'lg',
 	},
 }
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
 	args: {
 		asChild: true,
-		children: <p>Text with p tag</p>,
+		children: <h1>Heading with h1</h1>,
 	},
 	argTypes: {
 		children: {
