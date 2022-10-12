378aafc729d9e83e6f3fcb14b1ce853ea1eaea84
riosjr <riosjr@gmail.com>
2022-10-12 00:22:25 -0300

    feat: storybook dark theme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,5 +21,6 @@ module.exports = {
 		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'never'],
+		'react/no-unescaped-entities': 0,
 	},
 }
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './button.css';
+import React from 'react'
+import './button.css'
 
 interface ButtonProps {
   /**
@@ -28,21 +28,21 @@ interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
+	primary = false,
+	size = 'medium',
+	backgroundColor,
+	label,
+	...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+	const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary'
+	return (
+		<button
+			type="button"
+			className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+			style={{ backgroundColor }}
+			{...props}
+		>
+			{label}
+		</button>
+	)
+}
