a704188442147f9a0ef321a0dbe4f5d3abe56df2
riosjr <riosjr@gmail.com>
2022-10-12 12:10:34 -0300

    feat: import font and colors tokens

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import './styles/global.css'
 
 export function App() {
-	return <h1 className="text-6xl">Teste</h1>
+	return <h1 className="text-2xl bg-cyan-500 hover:bg-cyan-300">Teste</h1>
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,26 @@
 module.exports = {
 	content: ['./src/**/*.tsx'],
 	theme: {
+		// changing all the fonts and colors.
+		// But it could be useful do extend in some cases.
+		fontSize: {
+			xs: 14,
+			sm: 16,
+			md: 18,
+			lg: 20,
+			xl: 24,
+			'2xl': 32,
+		},
+		colors: {
+			'gray-900': '#121214',
+			'gray-800': '#202024',
+			'gray-400': '#7C7C8A',
+			'gray-200': '#C4C4CC',
+			'gray-100': '#E1E1E6',
+
+			'cyan-500': '#61DAFB',
+			'cyan-300': '#9BE1FB',
+		},
 		extend: {
 			fontFamily: {
 				sans: 'Inter, sans-serif',
