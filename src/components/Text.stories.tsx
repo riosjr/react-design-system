4a9ba6dd13a0d06ca0e4b2771d632bc07d43b7ff
riosjr <riosjr@gmail.com>
2022-10-12 13:23:13 -0300

    base text component: more configs

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,29 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
+
+//global configurations
 
 export default {
 	title: 'Components/Text',
 	component: Text,
+	args: {
+		children: 'Lorem ipsum.',
+		size: 'md',
+	},
+} as Meta<TextProps>
+
+// varitans configurations
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+	args: {
+		size: 'sm',
+	},
 }
 
-export const Default = {}
+export const Large: StoryObj<TextProps> = {
+	args: {
+		size: 'lg',
+	},
+}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
-export function Text() {
-	return <h1>TESTE</h1>
+import { clsx } from 'clsx'
+
+export interface TextProps {
+	size?: 'sm' | 'md' | 'lg'
+	children: string
+}
+
+export function Text({ size = 'md', children }: TextProps) {
+	return (
+		<span
+			className={clsx('text-gray-100 font-sans', {
+				'text-xs': size === 'sm',
+				'text-sm': size === 'md',
+				'text-md': size === 'lg',
+			})}
+		>
+			{children}
+		</span>
+	)
 }
