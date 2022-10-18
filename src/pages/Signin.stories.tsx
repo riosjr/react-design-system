b1d0046f2723bedd119a05d54024fd14a49bac83
riosjr <riosjr@gmail.com>
2022-10-18 01:29:14 -0300

    state simulation

---
+++ src/pages/Signin.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Signin } from './Signin'
+
+//global configurations
+
+export default {
+	title: 'Pages/Sign in',
+	component: Signin,
+	args: {},
+	argTypes: {},
+} as Meta
+
+// varitans configurations
+
+export const Default: StoryObj = {}
